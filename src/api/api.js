c111e897cd7e1089d02d1b2969f67a64c245cbdc
Jesus Axel Gonzalez Basilio <2312jagb@gmail.com>
2025-07-06 00:17:36 -0600

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://gestionprestamos-server.onrender.com/api';
+const API_URL = "https://sistemaonclicklock.onrender.com/api/";
 //const API_URL = 'http://localhost:8000/api';
 
 
